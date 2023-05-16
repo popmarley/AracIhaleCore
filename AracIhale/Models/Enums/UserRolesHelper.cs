f913dee04dace35997bde6b8d32a7a2641085596
pop marley <hsynozguven@hotmail.com>
2023-05-17 00:01:54 +0300

    Guncelleme(role  adı yazıyor)

---
+++ AracIhale/Models/Enums/UserRolesHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AracIhale.UI.Models.Enums
+{
+	
+	public enum UserRoles
+	{
+		Admin = 1,
+		Kullanici = 2,
+		PlusAdmin = 3
+	}
+	public static class UserRolesHelper
+	{
+		public static string GetRoleName(int roleId)
+		{
+			return Enum.GetName(typeof(UserRoles), roleId);
+		}
+	}
+
+
+}
